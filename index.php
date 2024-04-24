1b9be41e295ff27918bad68bf481d37eb65bdc7f
Simon Cik <cik@openlab.sk>
2024-04-24 12:50:41 +0200

    learning

--- index.php
+++ index.php
@@ -27,6 +27,82 @@ function Test() {
 Test();
  */
 
+
+
+$x = 5;
+$y = 10;
+
+function myTest() {
+  global $x, $y;   //cez global pristupujem ku vsetkym premennym inak by to davalo error
+  $y = $x + $y;
+}
+
+echo strlen("Hello world!"); // The PHP strlen() function returns the length of a string.
+
+//The PHP str_word_count() function counts the number of words in a string.
+
+$x = "Hello World!";
+echo str_replace("World", "Dolly", $x);
+
+$y = (string) $a; //menime typ premennej v ktorej je int na string
+
+echo(rand(10, 100));  //random cislo
+ 
+if (5 > 3) {
+  echo "Have a good day!";
+}
+
+$t = date("H");
+
+if ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break; //break - dolezite
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
+$i = 1;
+while ($i < 6) {
+  echo $i;
+  $i++;
+}
+
+$i = 1;
+
+do {
+  echo $i;
+  $i++;
+} while ($i < 6);
+
+for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+
+
+
+
+
+
+
+
+
+
+
  
 
 
